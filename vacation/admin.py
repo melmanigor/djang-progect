e165f0c5270af184e70999c123e9e94b138c03b6
Igor-balamuth <igor@balamuth.co.il>
2025-06-04 21:36:21 +0300

    admin vacation

--- vacation/admin.py
+++ vacation/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Vacation, Country
 
-# Register your models here.
+@admin.register(Vacation)
+class VacationAdmin(admin.ModelAdmin):
+    list_display = ('country', 'description', 'start_date', 'end_date', 'photo', 'price', 'like_count')
+    list_filter = ('country',)
+    search_fields = ('country__name', 'description')
+
+    def photo(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="border-radius: 50%; object-fit: cover;">',
+                obj.image.url
+            )
+        return 'No image'
+    photo.short_description = 'Image'
+
+    def like_count(self, obj):
+        return obj.liked_by.count()
+    like_count.short_description = 'Likes'
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
